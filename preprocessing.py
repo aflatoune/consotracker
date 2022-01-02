fe489ee1e4e78bcae80d6e2c53a042b37bbec0f3
aflatoune <daniel.kharsa@gmail.com>
2022-01-02 22:55:33 +0100

    add yoy diff

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,15 @@
 from statsmodels.tsa.seasonal import STL
 
 
-def _deseasonalize(df, type):
-    if type == "stl":
+def _deseasonalize(df, mode):
+    if mode == "stl":
         stl = STL(df, seasonal=13)
         res = stl.fit()
         return res.trend
+
+    if mode == "yoy":
+        return df.diff(12)
+
 
 def _growth_wrt_date(df, date="2004-01-01"):
     base = df.loc[date][0]
