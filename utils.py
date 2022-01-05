a2254b89c138e0216272076f55290848a6bb5475
aflatoun <daniel.kharsa@gmail.com>
2022-01-05 17:04:28 +0000

    add doc / raise exceptions

--- utils.py
+++ utils.py
@@ -3,9 +3,26 @@ import logging as lg
 from pytrends.request import TrendReq
 
 
-def read_from_csv(csvfile):
-    df = pd.read_csv(csvfile, sep=";", encoding="UTF-8")
-    dict_kw = {sect: df for sect, df in df.groupby(by=df.columns[0])}
+def read_from_csv(csvfile, sep=';', encoding='UTF-8'):
+    """ 
+    Read csv containing the following columns : 'cnat_code', 'kw',
+    'cat', 'label'. Relies on pandas.read_csv.
+
+    Returns
+    -------
+    A dict with cnat_code (french sectors codes) as keys and pandas
+    DataFrames (containing keywords, categories and labels) as values.
+    """
+    df = pd.read_csv(csvfile, sep=sep, encoding=encoding)
+
+    if any(df.columns != ['cnat_code', 'kw', 'cat', 'label']):
+        raise ValueError('csv file must contain the following columns:',
+        'cnat_code, kw, cat, label')
+
+    if df.isnull().values.any():
+        raise ValueError("csv file contains missing values.")
+
+    dict_kw = {sect: df for sect, df in df.groupby(by=["cnat_code"])}
     return dict_kw
 
 
