8fb17038eaf891eeda408df4e4a194a85e1c18d2
aflatoune <daniel.kharsa@gmail.com>
2022-01-21 17:59:11 +0100

    define plot method

--- consotracker/models/_model.py
+++ consotracker/models/_model.py
@@ -1,35 +1,63 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
+from abc import ABC, abstractmethod
 
-class Model():
-    """
-    Parent class for all models
+
+class Model(ABC):
+    """Parent class for all models
     """
 
-    self.predicted_df = None
+    predicted_df = None
 
     def __init__(self, endog=None, exog=None):
         self.endog = endog
         self.exog = exog
-        if endog is not None and pd.infer_freq(endog) == "N":
-            raise ValueError("Unrecognised frequency for endog.")
-        if exog is not None and pd.infer_freq(exog) == "N":
-            raise ValueError("Unrecognised frequency for exog.")
-        if endog is not None and exog is not None:
-            if pd.infer_freq(endog) != pd.infer_freq(exog):
-                raise ValueError("endog and exog must have the same frequency.")
+        if endog is not None:
+            self._check_validity(endog)
+        if exog is not None:
+            self._check_validity(exog)
 
+    def _check_validity(self, data):
+        if "date" not in data.columns:
+            raise ValueError(
+                "Both endog and exog must contain a \"date\" column.")
+        else:
+            if not is_datetime(data["date"]):
+                raise ValueError("\"date\" must be of type datetime64.")
+
+    @abstractmethod
     def fit(self):
-        """abstract method
-        """
         pass
 
+    @abstractmethod
     def predict(self):
-        """abstract method
-        """
         pass
 
-    def plot(self):
-        predicted_values = self.predicted_values
-        if predicted_values is None:
-            raise ValueError("cannot call plot() before fit() and predict().")
+    def plot(self, start=None, figsize=None, **kwargs):
+        predicted_df = self.predicted_df
+        if predicted_df is None:
+            raise ValueError("cannot call plot() before predict().")
+
+        if figsize is None:
+            figsize = (11, 6)
+
+        title = kwargs.get(title, "")
+        pred_color = kwargs.get(pred_color, "darkorange")
+        obs_color = kwargs.get(obs_color, "darkblue")
+        xlabel = kwargs.get(xlabel, "")
+        ylabel = kwargs.get(ylabel, "")
+        anchor = [0.5, 0]
+
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.grid(visible=True, which="major")
+        ax.plot(predicted_df.date, predicted_df.obs, c=obs_color)
+        ax.plot(predicted_df.date, predicted_df.pred, c=pred_color)
+        ax.set_label(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.margins(x=0)
+        fig.autofmt_xdate(rotation=90)
+        plt.title(title)
+        plt.legend(["obs", "pred"], bbox_to_anchor=anchor,
+                   loc="lower center", ncol=2)
+        return ax
