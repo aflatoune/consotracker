88b663513dd84c7a7b7cc8627c754f1c189bed90
aflatoune <daniel.kharsa@gmail.com>
2022-01-28 22:34:40 +0100

    update __init__.py

--- consotracker/models/__init__.py
+++ consotracker/models/__init__.py
@@ -1,2 +1,4 @@
 from ._model import Model
 from ._linear import LinearRegression
+from ._penalized import PenalizedRegression
+from ._randomforest import RandomForest
---
+++ consotracker/utils/__init__.py
@@ -0,0 +1,2 @@
+from ._preprocessing import Processing
+from ._roue import ROUE
