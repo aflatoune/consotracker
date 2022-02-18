968e91aeef309bb0fd2f5e91cb1338c2e48d8161
aflatoune <daniel.kharsa@gmail.com>
2022-02-18 17:36:05 +0100

    read from json instead of csv

--- consotracker/utils/_download_data.py
+++ consotracker/utils/_download_data.py
@@ -1,65 +1,18 @@
 import pandas as pd
 import logging as lg
-from csv import DictReader
 from pytrends.request import TrendReq
 from pytrends.exceptions import ResponseError
 from dbnomics import fetch_series
 
 
-def read_kw_from_csv(csvfile, sep=';', encoding='UTF-8'):
-    """
-    Read csv containing the following columns : 'cnat_code', 'kw' (keyword),
-    'cat', 'label'. Relies on pandas.read_csv.
-
-    Returns
-    -------
-    A dict with cnat_code (french sectors codes) as keys and pandas
-    DataFrames (containing keywords, categories and labels) as values.
-    """
-    df = pd.read_csv(csvfile, sep=sep, encoding=encoding)
-
-    if any(df.columns != ['cnat_code', 'kw', 'cat', 'label']):
-        raise ValueError('csv file must contain the following columns:',
-                         'cnat_code, kw, cat, label')
-
-    if df.isnull().values.any():
-        raise ValueError("csv file contains missing values.")
-
-    dict_kw = {sect: df for sect, df in df.groupby(by=["cnat_code"])}
-    return dict_kw
-
-
-def read_dbcode_from_csv(csvfile,
-                         skip_first_row=True,
-                         sep=';',
-                         encoding='UTF-8'):
-    """
-    Read csv containing the sector codes as derived from the national accounts,
-    and the corresponding series codes to be downloaded from DBnomics.
-
-    Returns
-    -------
-    A dict with cnat_code (french sectors codes) as keys and codes to download
-    series as values.
-    """
-    dict_dbcodes = {}
-    reader = DictReader(open(csvfile, encoding=encoding), delimiter=sep,
-                        fieldnames=["k", "v1", "v2", "v3"])
-
-    if skip_first_row:
-        headers = next(reader)
-
-    for row in reader:
-        dict_dbcodes.update({row["k"]: [row["v1"], row["v2"], row["v3"]]})
-    return dict_dbcodes
-
-
 def download_gtrends(dict_kw, timeframe="all", geo="FR"):
     dict_dfs = {}
     pytrends = TrendReq()
-    for sect, df in dict_kw.items():
+    for sect, subdict in dict_kw.items():
         l = []
-        for kw, cat in zip(df["kw"], df["cat"]):
+        for subsubdict in subdict.values():
+            kw = subsubdict["kw"]
+            cat = subsubdict["cat"]
             try:
                 pytrends.build_payload(
                     [kw], cat=cat, timeframe=timeframe, geo=geo, gprop=''
@@ -83,14 +36,15 @@ def download_dbseries(dict_dbcodes, start="2004-01-01"):
         Indicates the begining of the series (must be in YYYY/MM/DD format).
     """
     dict_series = {}
-    for sect, codes in dict_dbcodes.items():
-        dataset_code = codes[0]
-        series_code = codes[1]
-        try:
-            dict_series[sect] = fetch_series(
-                'INSEE', dataset_code, series_code)
-        except ValueError:
-            lg.warning(f'Download failed for {dataset_code}/{series_code}.')
+    for sect, subdict in dict_dbcodes.items():
+        for subsubdict in subdict.values():
+            dataset_code = subsubdict["dataset_code"]
+            serie_code = subsubdict["serie_code"]
+            try:
+                dict_series[sect] = fetch_series(
+                    'INSEE', dataset_code, serie_code)
+            except ValueError:
+                lg.warning(f'Download failed for {dataset_code}/{serie_code}.')
 
     for sect, df in dict_series.items():
         if start is not None:
