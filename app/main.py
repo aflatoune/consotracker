81172d7d81334eebfb8bda54d944035e94561262
titigmr <thierry.gmr@outlook.fr>
2022-03-06 19:10:51 +0100

    correct cache and cols

--- app/main.py
+++ app/main.py
@@ -23,20 +23,24 @@ if __name__ == '__main__':
         dict_dbcodes = json.load(f2)
 
     sector_list = match_dict(dict_kw, dict_dbcodes)
-    dict_dfs, dict_series = st_serie.dl_data(dict_kw, dict_dbcodes)
+    if "dict_dfs" not in st.session_state or "dict_series" not in st.session_state:
+        dict_dfs, dict_series = st_serie.dl_data(dict_kw, dict_dbcodes)
+        st.session_state["dict_dfs"] = dict_dfs
+        st.session_state["dict_series"] = dict_series
 
-    cols = st.columns(len(sector_list))
-    for col, sector in zip(cols, sector_list):
-        with col:
-            st.subheader(f'{dict_dbcodes[sector]["label"]}')
-            graph = st.empty()
-            metrics = st.empty()
-            st_web.v_spacer(height=0, sb=False)
-            with st.spinner(text="Chargement"):
-                graph.empty()
-                fig, lm_model = st_serie.create_serie(
-                    dict_dfs, dict_series, DICT_MODELS[model_name], sector)
-            predicted_df = lm_model.predicted_df
-            predicted_df = predicted_df.round({"obs": 2, "pred": 2})
-            st_serie.plot_alt2(predicted_df, graph)
-            st_serie.add_metrics(predicted_df, metrics)
+    for sector in sector_list:
+        st.subheader(f'{dict_dbcodes[sector]["label"]}')
+        graph = st.empty()
+        metrics = st.empty()
+        st_web.v_spacer(height=0, sb=False)
+        with st.spinner(text="Chargement"):
+            graph.empty()
+            fig, lm_model = st_serie.create_serie(
+                dict(st.session_state["dict_dfs"]),
+                dict(st.session_state["dict_series"]),
+                DICT_MODELS[model_name], sector)
+        predicted_df = lm_model.predicted_df
+        predicted_df = predicted_df.round({"obs": 2, "pred": 2})
+        st_serie.plot_alt2(predicted_df, graph)
+        st_serie.add_metrics(predicted_df, metrics)
+        st_web.v_spacer(height=3)
